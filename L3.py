1fa1066fe0e43c39b1b9911223954b7b8c8a2987
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-10-09 19:21:34 +0600

    User Input (float)

--- L3.py
+++ L3.py
@@ -1,4 +1,4 @@
-base = float(input("The value of base is?"))
-height = float(input("The value of height is?"))
+base = float(input("The value of base is?\n=> "))
+height = float(input("The value of height is?\n=> "))
 area = (1/2)*base*height
-print("The area of the triangle is=", area)
+print("The area of the triangle is=\n=> ", area)
