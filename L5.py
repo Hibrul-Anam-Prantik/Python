fa043ad467776ff5b4e1fdaa178a33fe81274454
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-10-09 20:27:48 +0600

    printing in other ways

--- L5.py
+++ L5.py
@@ -1,6 +1,11 @@
 a=5
 b=6
-print(f"{a}+{b}= {a+b}")
+print(f"{a} + {b} = {a + b}")
 
 print("Prantik",end=". ")
 print("Learning python.")
+
+a = 4; b = 5
+print(f"{a} + {b} = {a + b}")
+print("My name is", end=" ")
+print("Prantik")
