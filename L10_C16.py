bf703af6be4440290ec9dbb57986cbb7fb8702af
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-10-11 04:17:47 +0600

    if-else flow

--- L10_C16.py
+++ L10_C16.py
@@ -1,19 +1,19 @@
-a=20
-b=10
-c=30
-if a>b:
-    if a>c:
+a = 20
+b = 10
+c = 30
+if a > b:
+    if a > c:
         print("A")
     else:
         print("A1")
 
-if b>a:
-    if b>c:
+if b > a:
+    if b > c:
         print("B")
     else:
         print("B1")
 else:
-    if c>b:
+    if c > b:
         print("C")
     else:
         print("C1")
