8265483ad3403e2a38ba6b7711a7b542bd4cbb3f
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-10-13 01:04:47 +0600

    array

--- L17_C23.py
+++ L17_C23.py
@@ -1,16 +1,16 @@
-aplhabet = ["a","b","c","d","e","f"]
-print(aplhabet)
-print(aplhabet[1])
-print(aplhabet[2])
-print(aplhabet[2:])
-print(aplhabet[-1])
-print(aplhabet[-2])
+alphabet = ["a","b","c","d","e","f"]
+print(alphabet)
+print(alphabet[0])
+print(alphabet[2])
+print(alphabet[2:])
+print(alphabet[-1])
+print(alphabet[-2])
 
-print("a" in aplhabet)
-print("h" in aplhabet)
-print("a" not in aplhabet)
-print("h" not in aplhabet)
+print("a" in alphabet)
+print("h" in alphabet)
+print("a" not in alphabet)
+print("h" not in alphabet)
 
-print(aplhabet + ["Apple",100])
+print(alphabet + ["Apple",100])
 
-print(aplhabet * 3)
+print(alphabet * 3)
