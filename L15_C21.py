4a88cfacb433fcbf725ecd2d8b1db3de4707d2ea
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-10-13 00:48:56 +0600

    while loop application

--- L15_C21.py
+++ L15_C21.py
@@ -1,26 +1,40 @@
-'''i=1
-sum=0
-while i<=5:
-    sum=sum+i
-    i=i+1
-print(sum)'''
+"""
+i = 1
+sum = 0
+while i <= 5:
+    sum =sum + i
+    i = i + 1
+print(sum)
+"""
 
-'''n = int(input("Enter the last term: "))
+'''
+n = int(input("Enter the last term: "))
 sum = 0
 i = 1
 while i <= n :
     sum = sum + i
     i = i + 1
-print(sum)'''
+print(sum)
+'''
 
 
 #2+4+6+......+n
-n = int(input("Enter the last term: "))
-sum = 0
+'''n = int(input("Enter the last term: "))
+summ = 0
 i = 2
 while i <= n :
-    sum = sum + i
+    summ = summ + i
     i = i + 2
-print(sum)
+print(summ)
+'''
 
-
+first = int(input("Enter the first term: "))
+last = int(input("Enter the last term: "))
+d = int(input("Enter the common difference: "))
+summation = 0
+i = first
+while i <= last:
+    print(summation ,"+", i, "=", summation + i)
+    summation += i
+    i += d
+print("Summation:", summation)
