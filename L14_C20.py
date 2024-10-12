b3b50f5cc7b559d41aa773d1ab957f958d08463b
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-10-13 00:37:06 +0600

    while loop

--- L14_C20.py
+++ L14_C20.py
@@ -1,14 +1,26 @@
-'''i=1
-while i<=10:
+"""
+i = 1
+while i <= 10:
     print(i)
-    i=i+1'''
+    i = i + 1
+"""
 
-'''i= 1
-while i<=10:
+'''
+i = 1
+while i <= 10:
     print("Bangladesh")
-    i=i+1'''
+    i = i + 1'''
 
-i=2
-while i<=100:
+'''
+i = 2
+while i <= 100:
     print(i)
-    i=i+2
+    i = i + 2
+'''
+
+i = 0
+
+while i < 10:
+    print(i, ": Stupid Loop")
+    # i = i +1
+    i += 1
