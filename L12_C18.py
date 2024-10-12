0b4f06e7bbb81e40ef561370cbe800948783d22e
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-10-13 00:19:45 +0600

    if-else-elif > and, or

--- L12_C18.py
+++ L12_C18.py
@@ -1,13 +1,14 @@
-a=20
-b=10
-c=80
-if a>b and a>c:
-    print("A is Max.")
-elif b>a and b>c:
-    print("B is Max.")
+a = 120
+b = 10
+c = 80
+if a > b and a > c:
+    print(str(a) + " is Max.")
+elif b > a and b > c:
+    print(str(b) + " is Max.")
 else:
-    print("C is Max.")
-X= input("Type any letter to test- ")
-if X=="a" or X== "e" or X=="i" or X=="o"or X=="u":
+    print(c, "is Max.")
+X = input("Enter a letter (LowerClass): ")
+if X == "a" or X== "e" or X=="i" or X=="o"or X=="u":
     print("Vowel.")
-else:print("Consonant.")
+else:
+    print("Consonant.")
