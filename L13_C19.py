16479a027b46d390ac95440596d51d9a64876e48
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-10-13 00:27:09 +0600

    if-else-elif

--- L13_C19.py
+++ L13_C19.py
@@ -1,16 +1,26 @@
-#a=int(input("Obtained marks- "))
+"""
+a = int(input("Obtained marks: "))
 
-'''a= 85
-if a>=80 and a<=100:
+if a >= 80 and a <= 100:
     print("A+")
-elif a>=70 and a<80:
+elif a >= 70 and a < 80:
     print("A")
-elif a>=60 and a<70:
-    print("A-")'''
-a=50
-if 80<=a<=100:
+elif a >= 60 and a < 70:
+    print("A-")
+"""
+
+a = 50
+if 80 <= a <= 100:
     print("A+")
-elif 80>a>=70:
+elif 80 > a >= 70:
     print("A")
+else:
+    print("Lower than A")
+
+a = 79
+if 80 <= a <= 100:
+    print("A+")
+elif 70 <= a < 80:
+    print("A-")
 else:
     print("Lower than A")
