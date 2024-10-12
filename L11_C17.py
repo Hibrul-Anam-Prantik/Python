05a5803c482f51fe6ebb2898b36ca7749ff311a9
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-10-13 00:09:28 +0600

    Apply if-else in print statement

--- L11_C17.py
+++ L11_C17.py
@@ -1,9 +1,17 @@
-a=90
-b=100
-'''if a>b:
+a = 90
+b = 100
+
+'''
+if a>b:
     print("A is the large number.")
 else:
     print("B")
 '''
-print("Max number is", "A" if b>a else "B")
+# usage of if-else in print()
+print("Largest number is", "B" if b > a else "A")
 
+
+'''if a < b:
+    print("Smallest number is A" if a < b else "Smallest number is B")
+'''
+print("Smallest number is", "A" if a < b else "B")
