9fa33059b8eb6bb552176099ce057defbdaad87c
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-10-13 01:00:38 +0600

    while loop > break/ continue

--- L16_C22.py
+++ L16_C22.py
@@ -1,15 +1,17 @@
-'''i =  1
-while i <= 100:
-    if i == 10:
+i =  1
+while i <= 10:
+    if i == 6:
         break
     print(i)
-    i = i + 1'''
-
+    i = i + 1
+print("=============")
 
 i = 1
-while i <= 100:
-    if i == 20:
+while i <= 10:
+    if i == 9:
+        i += 1
         continue
     print(i)
-    i = i +1
+    i = i + 1
+print("=============")
 
