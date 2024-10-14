9577af92f2a0a4f611dc59ccd12496a98bd10a0f
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-10-15 04:50:40 +0600

    Array > list

--- L19_C25.py
+++ L19_C25.py
@@ -1,10 +1,13 @@
 a = list(range(10))
 print(a)
+print("=============1=============")
 
 print(a[3])
+print("=============1=============")
 
 a = list(range(5,11))
 print(a)
+print("=============1=============")
 
 a = list(range(5,50,5))
 print(a)
