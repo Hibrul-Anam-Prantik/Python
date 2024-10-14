7bbbe330c7bf1df44ec9a9d6f263474f9a102e3c
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-10-14 23:35:54 +0600

    Array manipulation

--- L18_C24.py
+++ L18_C24.py
@@ -1,32 +1,44 @@
-'''subjects = ["A", "B", "C", "D", "E"]
+subjects = ["A", "B", "C", "D", "E"]
 print(len(subjects))
+print("=============================")
 
 subjects.append("F")
 print(subjects)
+print("=============================")
 
 subjects.insert(2, "b")
 print(subjects)
+print("=============================")
 
 subjects.remove("b")
-print(subjects)'''
+print(subjects)
+print("=============================")
 
 subjects = ["B", "D", "a", "C", "A", "c", "b", 'd' ]
 subjects.sort()
 print(subjects)
+print("=============================")
 
 subjects.pop()
+print(subjects)
+print("=============================")
+
 subjects.pop()
 print(subjects)
+print("=============================")
 
 '''subjects.clear()
 print(subjects)'''
+print("=============================")
 
 subjects2 = subjects.copy()
 print(subjects2)
+print("=============================")
 
 subjects3 = [30,50,20,40,20,20,20]
 pos = subjects3.index(40)
 print(pos)
+print("=============================")
 
 a = subjects3.count(20)
 print(a)
