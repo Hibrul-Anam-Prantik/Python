d0ddb774db0768b38a372914f4acd79a8f878108
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-10-14 23:45:58 +0600

    Array > manipulation

--- L18_C24.py
+++ L18_C24.py
@@ -1,44 +1,53 @@
 subjects = ["A", "B", "C", "D", "E"]
-print(len(subjects))
-print("=============================")
+print("Array:", subjects)
+print("Length:",len(subjects))
+print("=============1=============")
 
 subjects.append("F")
-print(subjects)
-print("=============================")
+print("Updated Array:",subjects)
+print("Updated Length:",len(subjects))
+print("=============2=============")
 
 subjects.insert(2, "b")
-print(subjects)
-print("=============================")
+print("Updated Array:",subjects)
+print("Updated Length:",len(subjects))
+print("=============3=============")
 
 subjects.remove("b")
-print(subjects)
-print("=============================")
+print("Updated Array:",subjects)
+print("Updated Length:",len(subjects))
+print("=============4=============")
 
 subjects = ["B", "D", "a", "C", "A", "c", "b", 'd' ]
 subjects.sort()
-print(subjects)
-print("=============================")
+print("\nNew Sorted Array:",subjects)
+print("Length:",len(subjects))
+print("=============5=============")
 
 subjects.pop()
-print(subjects)
-print("=============================")
+print("Updated Array:",subjects)
+print("Updated Length:",len(subjects))
+print("=============6=============")
 
 subjects.pop()
-print(subjects)
-print("=============================")
+print("Updated Array:",subjects)
+print("Updated Length:",len(subjects))
+print("=============7=============")
 
 '''subjects.clear()
 print(subjects)'''
-print("=============================")
+
+print("=============8=============")
 
 subjects2 = subjects.copy()
-print(subjects2)
-print("=============================")
+print("Copied Array:",subjects2)
+print("Length:",len(subjects))
+print("=============9=============")
 
 subjects3 = [30,50,20,40,20,20,20]
 pos = subjects3.index(40)
 print(pos)
-print("=============================")
+print("=============X=============")
 
 a = subjects3.count(20)
 print(a)
