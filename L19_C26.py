0a95cef9cdf285d4f358436fe31eadf4cb911620
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-10-17 00:38:09 +0600

    Array > list > indexing > for-while

--- L19_C26.py
+++ L19_C26.py
@@ -1,15 +1,29 @@
 a = [10,20,30,40,50]
-#print(a)
+print(a)
+print("=============1=============")
 
-"""index = 0
+index = 0
 n = len(a)
-while index < n :
-    print(a[index])
-    index = index + 1"""
+print("Length:",n)
+print("=============2=============")
 
-"""for x in a :
-    print(x)"""
+while index < n:
+    print(a[index], end = "  ")
+    index = index + 1
+print()
+print("=============3=============")
+
+for x in a:
+    print(x, end = "  ")
+print()
+print("=============4=============")
+
 sum = 0
 for x in a:
-    sum = sum + x
-print(sum)
+    sum += x
+print("Summation: " + str(sum))
+print("=============+=============")
+
+for i in range(0, n):
+    print(str(i + 1) + ". " + str(a[i]))
+print()
