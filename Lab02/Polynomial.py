2dc72daa4ec1237787772b0f86f9067bb204dbeb
Hibrul Anam Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2025-11-03 22:27:56 +0600

    tbc

---
+++ Lab02/Polynomial.py
@@ -0,0 +1,20 @@
+oeff = [1, 0, 2, 0, 5] # coefficients of the polynomial
+n = len(coeff) - 1 # degree. Remember: number of coeff = degree + 1
+
+'''
+For a single value of x, finding p(x)
+
+Note that this is an example of block comment in python. A block comment
+starts with three ' and ends with three '.
+'''
+
+x = 6.0
+p_x = 0.0
+
+#Your code HERE
+
+
+x_arr = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0]
+p_x_arr = []
+
+#Your code HERE
